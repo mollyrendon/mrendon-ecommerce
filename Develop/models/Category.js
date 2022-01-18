a41448e7b770fc8d174c7ff473db70bfa8d052f3
Molly Wobbles <molly.rendon@outlook.com>
2022-01-18 17:19:28 -0500

    Updated README and filled out columns on all models files

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      tpye: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # E-commerce Back End Starter Code
+
+# Purpose
+
+An application designed
+
+# Built With
+
+Express
+MySQL
+
+# Contribution
+
+Made by Molly Rendon
