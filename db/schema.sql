282c212a8f563d8baa6c8c974f2f07448848cd9a
Molly Wobbles <molly.rendon@outlook.com>
2022-01-18 20:16:01 -0500

    Updated Category.findAll function

--- db/schema.sql
+++ db/schema.sql
@@ -8,27 +8,3 @@ CREATE DATABASE ecommerce_db;
 
 
 
-/*USE ecommerce_db;
-
-CREATE TABLE category (
-    id INT AUTO_INCREMENT,
-    category_name VARCHAR(30),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE product (
-    id INT AUTO_INCREMENT,
-    product_name VARCHAR(30),
-    price DECIMAL,
-    stock INT,
-    category_id INT
-);
-
-CREATE TABLE tag (
-    id INT AUTO AUTO_INCREMENT,
-    tag_name, 
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE product
-*/
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,25 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+/*Product Name:
+This piece of code uses a function called Category.findAll() which takes an object with two keys: include and model, where the value of include is set to {model: Product} and attributes is set to ['product_name].
+The function then uses .then() to return the data as JSON after it has been found.  Simply put, the code attempts to retrieve all products from the database and then returns them as JSON.  If there are no records found
+for the criteria then a 500 error message will be sent back.  
+*/
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    }
+  )
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
